a99c8f47372378ade7a60a1b017d326ee6256e52
Pete Smaluck <petesmaluck@gmail.com>
2015-06-14 18:49:07 -0400

    Updated styles

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,1 +1,34 @@
 //= require_tree .
+
+(function($) {
+
+$(document).ready(bindReadMore);
+
+  function bindReadMore() {
+    var $button = $('.blog-link');
+
+    $($button).click(function(e){
+      e.preventDefault();
+    });
+
+    $($button)
+      .off('click', handleReadMore)
+      .on('click', handleReadMore);
+  }
+
+  function handleReadMore() {
+    var fetch  = $(this).attr("href");
+
+    $.ajax({
+      type: 'GET',
+      url: fetch
+    })
+    .done(function(data) {
+      $('.intro').remove();
+      $('.current-post').remove();
+      $(data).find('p').addClass('current-post').appendTo($('.new-post'));
+      
+    });
+  }
+
+})(jQuery);
